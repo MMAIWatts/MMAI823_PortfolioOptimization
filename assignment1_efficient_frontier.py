24a7900f9debfced6076160650c53b6f09019b53
nafisasghari <nafiseh1365@gmail.com>
2019-12-15 19:13:26 -0500

    modify code

--- assignment1_efficient_frontier.py
+++ assignment1_efficient_frontier.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plt
 import pandas_datareader as pdr
 from scipy.optimize import minimize
 
+
 # get adjusted closing prices of selected stocks with yahoo
-stocks = ['GOOGL', 'BNS.TO', 'CNQ.TO', 'KXS.TO']
+stocks = ['GOOG', 'NKE', 'LMT', 'CAT' , 'TSLA', 'AMZN', 'TMO' , 'CSIQ' , 'HPE', 'AMD' , 'BHC', 'ROK' , 'TTWO', 'DELL', 'MU']
+
 source = 'yahoo'
-start = '2016-01-01'
-end = '2019-10-31'
+start = '2017-01-01'
+end = '2019-11-30'
 
 
 #Retrieve stock price data
@@ -17,6 +19,7 @@ data = pd.DataFrame()
 for symbol in stocks:
         data[symbol] = pdr.get_data_yahoo(symbol,  start=start, end=end)['Adj Close']
         
+#data.to_csv('stocks.csv')
 
 #daily historical returns for each stock (S(i) - S(i-1))/ S(i-1)) - use for calculating covariance matrix 
 daily_historical_returns = data.pct_change()
@@ -24,11 +27,13 @@ daily_historical_returns = data.pct_change()
 #Calculate Returns
 #according to Bloomberg’s estimate
 #according to yahoo Finance
-betas = []
-for stock in stocks:
+# betas = []
+# for stock in stocks:
     
-    beta = pd.read_html(f'https://ca.finance.yahoo.com/quote/{stock}?p={stock}')[1].iloc[1,1]
-    betas.append(float(beta))
+#     beta = pd.read_html(f'https://ca.finance.yahoo.com/quote/{stock}?p={stock}')[1].iloc[1,1]
+#     betas.append(float(beta))
+
+betas = [1.02, 0.83, 0.95, 1.51, 0.58, 1.52, 1.12, 2.04, 1.53, 3.08, 0.89, 1.44, 0.69, 0.74, 1.8]
 
 #risk free interest rate is 1.8% & expected return on the index is 7.8%
 risk_free_rate = 1.8 /100
@@ -38,7 +43,7 @@ index = 7.8 /100
 returns = risk_free_rate + np.array(betas) * ( index - risk_free_rate)
 
 #Creating some random portfolios
-num_portfolios = 3000
+num_portfolios = 2000
 
 all_weights = np.zeros((num_portfolios,len(stocks)))
 ret_arr = np.zeros(num_portfolios)
@@ -65,12 +70,6 @@ for n in range(num_portfolios):
     # Sharpe Ratio
     sharpe_arr[n] = ret_arr[n]/std_arr[n]
 
-# plt.figure(figsize=(12,8))
-# plt.scatter(vol_arr,ret_arr,c=sharpe_arr,cmap='plasma_r' , edgecolors='black')
-# plt.colorbar(label='Sharpe Ratio')
-# plt.xlabel('Standard Deviation')
-# plt.ylabel('Return')
-# plt.show()
 
 #Takes in weights, returns array or return,standard deviation, sharpe ratio 
 def get_ret_std_sr(weights):
@@ -93,7 +92,7 @@ init_guess = [1/len(stocks)] *len(stocks)
 bounds = tuple((0,1) for asset in range(len(stocks)))
 
 # Create a linspace number of points to calculate x
-frontier_returns = np.linspace(min(ret_arr),max(ret_arr),10) 
+frontier_returns = np.linspace(min(ret_arr),max(ret_arr),20) 
 
 frontier_std = []
 
@@ -134,9 +133,10 @@ init_guess = [1/len(stocks)] *len(stocks)
 bounds = tuple((0,1) for asset in range(len(stocks)))
 
 # Create a linspace number of points to calculate x
-frontier_returns = np.linspace(min(ret_arr),max(ret_arr),10) 
+frontier_returns = np.linspace(min(ret_arr),max(ret_arr),20) 
 
 frontier_std_with_RF = []
+weights_frintier = []
 
 for possible_return in frontier_returns:
     
@@ -147,8 +147,10 @@ for possible_return in frontier_returns:
     result = minimize(minimize_risk,init_guess,method='SLSQP',bounds=bounds,constraints=constraints)
     
     frontier_std_with_RF.append(result['fun'])
+    weights_frintier.append(result['x'])
 
 plt.figure(figsize=(12,8))
+
 plt.scatter(std_arr,ret_arr,c=sharpe_arr,cmap='plasma_r'  , alpha = 0.6)
 plt.colorbar(label='Sharpe Ratio')
 plt.xlabel('Standard Deviation')
@@ -156,11 +158,22 @@ plt.ylabel('Return')
 
 # Add frontier line
 plt.plot(frontier_std,frontier_returns,'b-',linewidth=2)
-
 plt.plot(frontier_std_with_RF,frontier_returns,'g--',linewidth=4)
 plt.show()
 
 
+plt.figure(figsize=(8,6))
 
+plt.xlabel('Standard Deviation')
+plt.ylabel('Return')
 
+# Add frontier line
+plt.plot(frontier_std,frontier_returns,'b-',linewidth=2)
+plt.plot(frontier_std_with_RF,frontier_returns,'g--',linewidth=4)
+plt.show()
 
+results = pd.DataFrame({'return' :frontier_returns, 'risk':frontier_std_with_RF, 'w': weights_frintier} )
+w = pd.DataFrame(results['w'].values.tolist(), columns=stocks)
+results = results.join(w).drop('w' , axis = 1).round(3)
+
+#results.to_csv('results.csv')
